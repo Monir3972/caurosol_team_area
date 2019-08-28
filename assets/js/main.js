f9820a0dcbd42ec5ab5766dc7bd4fdd96cbb466b
moniruzzaman <monircse3973@gmail.com>
2019-08-28 22:24:35 +0600

    caurosol team section

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+     
+     $(".client-testominal-caurosol").owlCarousel({
+     	items:3,
+     	margin:30,
+     	nav:false,
+     	// dots:true,
+     	loop:true
+     });
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
